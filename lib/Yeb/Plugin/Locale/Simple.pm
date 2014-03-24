2f42119c051266c1012d4d7323e535084b220c8a
Torsten Raudssus <torsten@raudss.us>
2014-03-25 00:34:13 +0100

    First release, functions available, not more yet

--- lib/Yeb/Plugin/Locale/Simple.pm
+++ lib/Yeb/Plugin/Locale/Simple.pm
@@ -1,5 +1,16 @@
-use strict;
-use warnings;
 package Yeb::Plugin::Locale::Simple;
+# ABSTRACT: Yeb Plugin for Locale::Simple connection
+
+use Moo;
+use Locale::Simple;
+
+has app => ( is => 'ro', required => 1 );
+
+sub BUILD {
+  my ( $self ) = @_;
+  for my $func (@Locale::Simple::EXPORT) {
+    $self->app->register_function($func,sub { &{\&{$func}}(@_) });
+  };
+}
 
 1;
