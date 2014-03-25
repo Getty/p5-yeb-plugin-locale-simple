a59499cc38db47088fa7d4bdf82a4cf4ef04b9f5
Torsten Raudssus <torsten@raudss.us>
2014-03-25 01:50:31 +0100

    Added ability to define locales directory on plugin configuration

--- lib/Yeb/Plugin/Locale/Simple.pm
+++ lib/Yeb/Plugin/Locale/Simple.pm
@@ -6,11 +6,17 @@ use Locale::Simple;
 
 has app => ( is => 'ro', required => 1 );
 
+has locales => (
+	is => 'ro',
+	predicate => 1,
+);
+
 sub BUILD {
   my ( $self ) = @_;
   for my $func (@Locale::Simple::EXPORT) {
     $self->app->register_function($func,sub { &{\&{$func}}(@_) });
   };
+  l_dir($self->locales) if $self->has_locales;
 }
 
 1;
